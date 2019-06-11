e1d63da81ba43415eee82767dcecdf33a5860802
ionfury <ionfury@gmail.com>
2019-06-11 13:36:27 -0400

    Update main.tf

--- least-privileged-sp/pre-reqs/main.tf
+++ least-privileged-sp/pre-reqs/main.tf
@@ -1,23 +1,23 @@
 module "vnet" {
     source = "vnet"
 
-    resource_group_name="tmp-lpsp-vnet"
+    resource_group_name="ion-vnet"
     resource_group_location="eastus"
-    network_name="lpsp-network"
+    network_name="ion-network"
 }
 
 module "acr" {
     source = "acr"
 
-    resource_group_name="tmp-lpsp-acr"
+    resource_group_name="ion-acr"
     resource_group_location="eastus"
-    acr_name="lpspacr"
+    acr_name="ionacr"
 }
 
 module "ips" {
   source = "public-ips"
   
-    resource_group_name="tmp-lpsp-ip"
+    resource_group_name="ion-ip"
     resource_group_location="eastus"
-    public_ip_name="tmp-lpsp-ip"
+    public_ip_name="ion-ip"
 }
